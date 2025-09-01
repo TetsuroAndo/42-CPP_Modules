8dfcef6bf1199da465942bcde38acf9bb6bd6d5b
TetsuroAndo <tetsuro997@gmail.com>
2025-09-01 15:49:41 +0900

    fix: add 64bit arch preprocessor

--- 6/ex01/Serializer.hpp
+++ 6/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 14:23:03 by teando            #+#    #+#             */
-/*   Updated: 2025/09/01 15:01:10 by teando           ###   ########.fr       */
+/*   Updated: 2025/09/01 15:39:07 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 #include <iostream>
 #include "Data.hpp"
 
-#if defined(__x86_64__) || defined(_M_X64) || defined(__aarch64__)
-typedef unsigned long uintptr_t; // 64bit
+#if defined(__x86_64__) || defined(_M_X64)			/* x86-64 */ \
+ || defined(__aarch64__) || defined(_M_ARM64)		/* ARM64  */ \
+ || defined(__ppc64__) || defined(__powerpc64__)	/* PowerPC64 */ \
+ || defined(__ia64__)								/* Itanium */ \
+ || defined(__sparc64__)							/* SPARC64 */ \
+ || defined(__LP64__)								/* LLP64 Data Model */ \
+ || defined(_WIN64)									/* Windows LLP64 */
+typedef unsigned long uintptr_t;
 #else
-typedef unsigned int uintptr_t;  // 32bit
+typedef unsigned int uintptr_t;
 #endif
 
 class Serializer {
