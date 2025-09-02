8d0192cd16c6d2c51f4f3fe998ec814494454f41
Tetsuro Ando <tetsuro997@gmail.com>
2025-09-03 01:06:50 +0900

    create 7/ex01

--- 7/ex01/iter.hpp
+++ 7/ex01/iter.hpp
@@ -6,16 +6,24 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 21:30:35 by teando            #+#    #+#             */
-/*   Updated: 2025/09/01 22:51:25 by teando           ###   ########.fr       */
+/*   Updated: 2025/09/02 23:33:53 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
 #include <cstddef>
 
-template <class T>
+template <typename T, typename F>
 void iter(T *arr, size_t len, F f) {
+	for (size_t i = 0; i < len; ++i) {
+		f(arr[i]);
+	}
+}
+
+template <typename T, typename F>
+void iter(const T *arr, size_t len, F f) {
 	for (size_t i = 0; i < len; ++i) {
 		f(arr[i]);
 	}
--- 7/ex01/main.cpp
+++ 7/ex01/main.cpp
@@ -6,7 +6,28 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 21:29:40 by teando            #+#    #+#             */
-/*   Updated: 2025/09/01 21:29:41 by teando           ###   ########.fr       */
+/*   Updated: 2025/09/02 04:44:32 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+namespace {
+	template <class T>
+	void printElement(const T &e) {
+		std::cout << e << std::endl;
+	}
+}
+
+int main() {
+	int arr[] = {1, 2, 3, 4, 5};
+	std::string strArr[] = {"Hello", "World", "!"};
+
+	std::cout << "int array:" << std::endl;
+	iter(arr, 5, printElement<int>);
+
+	std::cout << "string array:" << std::endl;
+	iter(strArr, 3, printElement<std::string>);
+
+	return 0;
+}
