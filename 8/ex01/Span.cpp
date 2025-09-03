3539460f4ba3fddb18bfe24d22d57865ff0f0710
TetsuroAndo <tetsuro997@gmail.com>
2025-09-04 05:56:09 +0900

    update

--- 8/ex01/Span.cpp
+++ 8/ex01/Span.cpp
@@ -6,69 +6,59 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 23:52:15 by teando            #+#    #+#             */
-/*   Updated: 2025/09/04 00:57:59 by teando           ###   ########.fr       */
+/*   Updated: 2025/09/04 04:57:28 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span() : _N(0), _vec() {}
-Span::Span(unsigned int N) : _N(N), _vec() {}
-Span::Span(const Span& other) : _N(other._N), _vec(other._vec) {}
+Span::Span() : _n(0), _vec() {}
+Span::Span(unsigned int n) : _n(n), _vec() {}
+Span::Span(const Span& other) : _n(other._n), _vec(other._vec) {}
 Span& Span::operator=(const Span& other) {
 	if (this != &other) {
-		_N = other._N;
+		_n = other._n;
 		_vec = other._vec;
 	}
 	return *this;
 }
 Span::~Span() {}
 
-void Span::addNumber(int number) {
-	if (_vec.size() >= _N) {
+void Span::addNumber(int n) {
+	if (_vec.size() >= _n) {
 		throw std::out_of_range("Span is full. Cannot add more numbers.");
 	}
-	_vec.push_back(number);
+	_vec.push_back(n);
 }
 
-int Span::shortestSpan() const {
+unsigned int Span::shortestSpan() const {
 	if (_vec.size() <= 1) {
 		throw std::logic_error("Not enough numbers to find a span.");
 	}
 
-	// 元のコンテナを破壊しないようにコピーを作成
-	std::vector<int> sorted = _vec;
-	std::sort(sorted.begin(), sorted.end());
+	// 内部コンテナのコピーを作成してソートする（元の順序を保持する場合）
+	// もしくは、元のコンテナを直接ソートしても良い
+	std::vector<int> sortedNumbers = _vec;
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
 
-	long long minSpan = std::numeric_limits<long long>::max();
+	unsigned int minSpan = std::numeric_limits<unsigned int>::max();
 
-	// ソート済みのベクターで隣接する要素の差を比較
-	for (size_t i = 1; i < sorted.size(); ++i) {
-		long long diff = static_cast<long long>(sorted[i]) - sorted[i-1];
-		if (diff < minSpan) {
-			minSpan = diff;
+	// ソート済みのベクターで隣接する要素の差を計算
+	for (size_t i = 1; i < sortedNumbers.size(); ++i) {
+		unsigned int span = sortedNumbers[i] - sortedNumbers[i - 1];
+		if (span < minSpan) {
+			minSpan = span;
 		}
 	}
-	return static_cast<int>(minSpan);
+	return minSpan;
 }
 
-int Span::longestSpan() const {
+unsigned int Span::longestSpan() const {
 	if (_vec.size() <= 1) {
 		throw std::logic_error("Not enough numbers to find a span.");
 	}
 
-	// std::minmax_element を使うと一度のスキャンで最小値と最大値を見つけられる
-	std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator> result;
-	result = std::minmax_element(_vec.begin(), _vec.end());
-
-	return (*result.second - *result.first);
-}
-
-template <typename I>
-void addRange(I first, I last) {
-	if (std::distance(first, last) + _vec.size() > _N) {
-		throw std::out_of_range("Cannot add range: not enough capacity.");
-	}
-	// 指定されたイテレータ範囲の要素をコンテナの末尾に挿入
-	_vec.insert(_vec.end(), first, last);
+	std::vector<int>::const_iterator minIt = std::min_element(_vec.begin(), _vec.end());
+	std::vector<int>::const_iterator maxIt = std::max_element(_vec.begin(), _vec.end());
+	return (*maxIt - *minIt);
 }
--- 8/ex01/Span.hpp
+++ 8/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 23:52:19 by teando            #+#    #+#             */
-/*   Updated: 2025/09/04 00:57:25 by teando           ###   ########.fr       */
+/*   Updated: 2025/09/04 04:56:35 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <stdexcept>
+#include <exception>
 #include <limits>
 
 class Span {
 private:
-	unsigned int _N;
+	unsigned int _n;
 	std::vector<int> _vec;
 
 public:
 	Span();
-	Span(unsigned int N);
+	Span(unsigned int n);
 	Span(const Span& other);
 	Span& operator=(const Span& other);
 	~Span();
 
 	void addNumber(int n);
-	int shortestSpan() const;
-	int longestSpan() const;
+	unsigned int shortestSpan() const;
+	unsigned int longestSpan() const;
 
-	template <typename T> void addRange(T begin, T end);
+	template <typename I>
+	void addRange(I begin, I end) {
+		if (static_cast<unsigned int>(std::distance(begin, end)) + _vec.size() > _n) {
+			throw std::out_of_range("Cannot add range: not enough capacity.");
+		}
+		// 指定されたイテレータ範囲の要素をコンテナの末尾に挿入
+		_vec.insert(_vec.end(), begin, end);
+	}
 };
-
